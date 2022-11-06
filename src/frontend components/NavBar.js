5fbf245d2ec228e129a977ff1a869076e18ca2ba
MPrathyusha01 <prathyusha4501@gmail.com>
2022-11-06 22:06:28 +0530

    06-11-2022 night

--- src/frontend components/NavBar.js
+++ src/frontend components/NavBar.js
@@ -11,34 +11,47 @@ export class NavBar extends Component {
 
     render() {
         return (
-            <div className="html">
-                <div className="header">
-                    <nav className="navbar">
-                        <a href="/" className="logo"> <i className="fas fa-heartbeat"></i> Care 4 All</a>
-                        <ul>
-                            <li>
-                                <a href="#">Home</a>
-                            </li>
-                            <li>
-                                <a href="#">Doctor</a>
-                            </li>
-                            <li>
-                                <a href="#">Service</a>
-                            </li>
-                            <li>
-                                <a href="#">About Us</a>
-                            </li>
-                            <li>
-                                <a href="#">Contact</a>
-                            </li>
+            <div className="header">
 
-                        </ul>
-                    </nav>
-                        
-                    </div>
-                    <div id="menu-btn" className="fas fa-bars">
-                </div>
+                <nav className="navbar">
+                <a href="/" className="logo"> <i className="fas fa-heartbeat"></i> Care 4 All</a>
+                    <a href="#">Home</a>
+                    <a href="#">Doctor</a>
+                    <a href="#">Service</a>
+                    <a href="#">About Us</a>
+                    <a href="#">Contact Us</a>
+                </nav>
+                <div id="menu-btn" class="fas fa-bars"></div>
             </div>
+            // <div className="html">
+            //     <div className="header html">
+            //         <nav className="navbar">
+            //             <a href="/" className="logo"> <i className="fas fa-heartbeat"></i> Care 4 All</a>
+            //             <ul>
+            //                 <li>
+            //                     <a href="#">Home</a>
+            //                 </li>
+            //                 <li>
+            //                     <a href="#">Doctor</a>
+            //                 </li>
+            //                 <li>
+            //                     <a href="#">Service</a>
+            //                 </li>
+            //                 <li>
+            //                     <a href="#">About Us</a>
+            //                 </li>
+            //                 <li>
+            //                     <a href="#">Contact</a>
+            //                 </li>
+
+            //             </ul>
+            //         </nav>
+
+            //         <div id="menu-btn" className="fas fa-bars">
+
+            //         </div>
+            //     </div>
+            // </div>
         );
     }
 }
